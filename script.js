9e7196a332b808dfa26c904ffb79246821991de1
DIVYAGARG0911 <gargdivya911@gmail.com>
2025-09-20 15:09:46 +0530

    Add javaScript file

---
+++ script.js
@@ -0,0 +1,54 @@
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b64cb317b2msh841ad4e2db87643p1746c0jsn9b5fea571941',
+		'x-rapidapi-host': 'weather-api138.p.rapidapi.com'
+	}
+};
+
+fetch('https://weather-api138.p.rapidapi.com/weather?city_name=Delhi', options)
+    .then(response => response.json())
+    .then((response) => {
+		console.log("Weather Data:", response)
+		temp = response.temp
+		feels_like = response.feels_like
+		temp_min = response.temp_min
+		temp_max = response.temp_max
+		humidity = response.humidity
+		speed = response.speed
+		deg = response.deg
+		sunrise = response.sunrise
+		sunset = response.sunset
+	})
+    .catch(err => console.error("Error:", err));
+
+
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b64cb317b2msh841ad4e2db87643p1746c0jsn9b5fea571941',
+		'x-rapidapi-host': 'weather-api138.p.rapidapi.com'
+	}
+};
+
+fetch('https://weather-api138.p.rapidapi.com/weather?city_name=Delhi', options)
+    .then(response => response.json())
+    .then((response) => {
+		console.log("Weather Data:", response)
+		temp = response.temp
+		feels_like = response.feels_like
+		temp_min = response.temp_min
+		temp_max = response.temp_max
+		humidity = response.humidity
+		speed = response.speed
+		deg = response.deg
+		sunrise = response.sunrise
+		sunset = response.sunset
+	})
+    .catch(err => console.error("Error:", err));
+
+
+
